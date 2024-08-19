a8338f43dbf9cff136042ab5a2a0a8c1248ef736
Roman_Tretiakov <romantre@wizardsdev.com>
2024-08-20 00:48:47 +0300

    Added HW15

--- Module8/module_8_1.py
+++ Module8/module_8_1.py
@@ -2,13 +2,15 @@ def add_everything_up(a, b):
     try:
         a + b
     except TypeError as err:
-        print(f'Возникла ошибка: {err}. Тип первого аргумента: {type(a)}, тип второго аргумента: {type(b)} ')
+        print(f'Возникла ошибка: {err}. Тип первого аргумента: {type(a)}, тип второго аргумента: {type(b)}')
         return f'{str(a)}{str(b)}'
     else:
         return round(a + b, 3)
     finally:
         if type(a) is int and type(b) is str:
             print(f'Эта функция может также складывать числа и строки, результат: {a + len(b)}')
+        elif type(a) is str and type(b) is int:
+            print(f'Эта функция может также складывать строки и числа, результат: {len(a) + b}')
 
 
 print(add_everything_up(123.456, 'строка'))
