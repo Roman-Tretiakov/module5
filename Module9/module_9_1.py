6dda0ac5d20bbc9adf6fe039f00c692033538fc8
Roman_Tretiakov <romantre@wizardsdev.com>
2024-08-26 16:34:11 +0300

    Updated HW17

--- Module9/module_9_1.py
+++ Module9/module_9_1.py
@@ -2,7 +2,7 @@ numbers = ['10', -1, '0', 100, 73, '-99', 5]
 
 
 def apply_all_func(int_list, *functions):
-    int_list = list(map(lambda el: int(el) if el is not isinstance(el, int) else el, int_list))
+    int_list = [int(el) if type(el) is str else el for el in int_list]
     dictionary = {}
 
     for func in functions:
